0f238de75a877b0f1b8547044ebdcd3e7bac0a94
stephanieboroshok <sb8485a@american.edu>
2019-10-30 09:57:16 -0400

    Initial commit

---
+++ Problem1 List Info.py
@@ -0,0 +1,11 @@
+#creating a module
+#On Blackboard, download the num_list_module.py file
+#Fill in code for each function defined, following the guidelines set
+#Be sure to create a Git repo and share progress on Github
+#Once created, get into groups of x and share your code with your group via Github.
+#In your own python file, utilize the functions defined in your partners’ module.
+#If there are any errors, help each other debug and troubleshoot
+
+import num_list_module
+intlist1 = [1,10,2,3,4,5]
+max1 = num_list_module.max()
---
+++ num_list_module.py
@@ -0,0 +1,45 @@
+
+def backwardslist(array):
+    """ Takes in a list and returns the list backwards"""
+
+def min(array):
+    """ Returns the lowest number in an list of numbers"""
+
+def firsthalfsum(array):
+    """Returns the sum of the first half of the list.
+        ***IF THE LIST HAS AN ODD NUMBER OF ELEMENTS, split the middle element in
+        half and add it to the sum.
+        """
+
+def divisibleby(array, divisor):
+    """ Returns each element divisible by the paramater 'divisor'
+
+    """
+
+def max(array):
+    """ Returns the highest number in a list of numbers """
+
+def avg(array):
+    """ Returns the average of a list of numbers"""
+
+def suprise():
+    """ Create a surprise function for the person that receives your code.
+        Feel free to get creative change parameters, print out shapes,  etc.
+
+        """
+
+
+################################
+####    BONUS FUNCTION       ###
+################################
+def gcd(array):
+    """ Returns the greatest common Divisor of a list of numbers """
+    """ Greatest Common Divisor is the greatest number that each number in the list is 
+        divisible by. 
+        EXAMPLE: [500, 50, 20] Greatest Common Divisor = 10
+                 [18, 30, 42]  Greatest Common Divisor = 6
+                 [33, 66, 99, 101] Greatest Common Divisor = 1
+                 
+                 """
+
+
