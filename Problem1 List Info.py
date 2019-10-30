e110e6e711e16486b4c09d24720f31f6fc0d8e0d
stephanieboroshok <sb8485a@american.edu>
2019-10-30 10:09:24 -0400

    added name per Narendra's request

--- Problem1 List Info.py
+++ Problem1 List Info.py
@@ -6,6 +6,8 @@
 #In your own python file, utilize the functions defined in your partners’ module.
 #If there are any errors, help each other debug and troubleshoot
 
+print ('Stephanie Boroshok')
+
 import num_list_module
 intlist1 = [1,10,2,3,4,5]
 max1 = num_list_module.max(intlist1)
